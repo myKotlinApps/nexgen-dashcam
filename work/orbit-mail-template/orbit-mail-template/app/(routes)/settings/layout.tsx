ffa782646b817988e61b957bed85984f5f400653
ali reza <myKotlinApps@gmail.com>
2026-08-04 16:21:47 +0330

    chore: ignore extracted work files

--- work/orbit-mail-template/orbit-mail-template/app/(routes)/settings/layout.tsx
+++
@@ -1,6 +0,0 @@
-'use client';
-import { SettingsLayoutContent } from '@/components/ui/settings-content';
-
-export default function SettingsLayout({ children }: { children: React.ReactNode }) {
-  return <SettingsLayoutContent>{children}</SettingsLayoutContent>;
-}
--- work/orbit-mail-template/orbit-mail-template/hooks/use-connections.ts
+++
@@ -1,8 +0,0 @@
-import { useTRPC } from '@/providers/query-provider';
-import { useQuery } from '@tanstack/react-query';
-
-export const useConnections = () => {
-  const trpc = useTRPC();
-  const connectionsQuery = useQuery(trpc.connections.list.queryOptions(void 0));
-  return connectionsQuery;
-};
